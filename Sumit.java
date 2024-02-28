13d5673d534cc137991dd99f6a037a888a8a1852
sumitpardeshi31 <161594507+sumitpardeshi31@users.noreply.github.com>
2024-02-28 17:26:08 +0530

    Add files via upload

---
+++ Sumit.java
@@ -0,0 +1,141 @@
+/*
+class Sumit {
+public static void main(String [] args)
+{
+System.out.println("bhau mana smarat");
+}
+}
+*/
+/*
+class Sumit{
+public static void main(String [] args)
+{
+System.out.println("i am a dharma director");
+}
+
+
+}
+*/
+
+/*
+class Sumit{
+public static void main(String[] args)
+{
+int number1 = 10;
+int number2 = 20;
+if (number1>5)
+{
+System.out.println("number1 is greater");
+}
+else 
+{
+System.out.println("number1 is not greater");
+}
+if(number2>number1)
+{
+System.out.println("bhai ki jay");
+}
+else
+{
+System.out.println("jay shree ram");
+}
+}
+}
+*/
+
+/*
+class Sumit{
+public static void main(String []args)
+{
+int [] x= {1,2,3,4};
+System.out.println(x.length);
+}
+
+}
+*/
+/*
+class Sumit{
+public static void main(String[] args)
+{
+int [] x= {1,2,3};
+System.out.println(x[0]);
+System.out.println(x[1]);
+System.out.println(x[2]);
+
+}
+}
+*/
+/*
+class Sumit{
+public static void main(String []args)
+{
+int number1 = 3;
+int number2 = 4;
+int answer= number1 + number2;
+System.out.println(answer);
+}
+}
+*/
+
+/*
+class Sumit
+{
+public static void main(String [] args)
+{
+int length= 2;
+int width= 4;
+int areaofrectangle= length * width;
+System.out.println(areaofrectangle);
+}
+
+}
+*/
+/*
+class Sumit
+{
+public static void main(String [] args)
+{
+String text = ("sumit bhai");
+System.out.println(text);
+}
+}
+*/
+/*
+class Sumit
+{
+public static void main(String [] args)
+{
+String text= ("hello");
+text= "java" + text;
+System.out.println(text);
+}
+}
+
+*/
+
+/*
+class Sumit
+{
+public static void main(String [] args)
+{
+String name= "bhaisahab";
+for (int i=0; i<=10; i++)
+{
+System.out.println(name);
+}
+}
+}
+*/
+
+class Sumit
+{
+public static void main(String []args)
+{
+for(int i=2; i<=10; i+=2)
+{
+System.out.println(i);
+}
+}
+
+}
+
